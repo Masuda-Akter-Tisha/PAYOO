e2e5952cf4e8df8edbad5568867b66a5d63079ec
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-03-22 23:21:12 +0600

    implemented buttons functions half

--- script/machine.js
+++ script/machine.js
@@ -16,8 +16,25 @@ function getBalance () {
     return Number(balance);
 }
 
-// machine -> set balance
+// machine value -> set balance
 function setBalance (value) {
     const balanceElement = document.getElementById ('balance');
     balanceElement.innerText = value;
+}
+
+// machine id -> hideAll -> showAll
+function showOnly (id) {
+    const addMoney = document.getElementById ('add-money');
+    const cashout = document.getElementById ('cashout');
+    // console.log (`Add-Money: ${addMoney},CashOut: ${cashout}`);
+
+    // hide all section
+    addMoney.classList.add ('hidden');
+    cashout.classList.add ('hidden');
+
+    // id oala element ta k tmi show koro
+    const selected = document.getElementById (id);
+    selected.classList.remove ('hidden');
+
+    console.log (id);
 }
